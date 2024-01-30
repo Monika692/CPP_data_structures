0aeadfd5d4d5180a7d92f83f88e24fc9247ff9aa
MONIKA_KUMARI <103176112+Monika692@users.noreply.github.com>
2024-01-30 14:48:17 +0530

    Create Information.cpp

---
+++ double ended Queue/Information.cpp
@@ -0,0 +1,7 @@
+We use double ended queue mostly in ticket purchasing line where the user can select the ticket and
+It performs like a queue but some time It happens that some body has purchased the ticket and sudden they come back to ask some thing on
+front of queue. In this scenario because they have already purchased the ticket so they have privilege to come and ask for any further query.
+So in these kind of scenario we need a data structure where according to requirement we add data from front. 
+And In same scenario user can also leave the queue from rear.
+
+Thats why we use double ended queue.
